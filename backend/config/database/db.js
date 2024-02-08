6d3e000ab2d251ef97dce381fea535861412d962
khatritilak956@gmail.com <ajaypokhrel981@gmail.com>
2024-02-08 12:01:38 +0545

    deployment config

--- backend/config/database/db.js
+++ backend/config/database/db.js
@@ -7,7 +7,7 @@ const setupDB = async () => {
   // console.log('I called',process.env.LOCAL_MONGO_URI)
   try {
     mongoose
-      .connect(process.env.LOCAL_MONGO_URI)
+      .connect(process.env.MONGO_URI)
       .then(() => console.log(colors.green("Connected to MongoDB")))
       .catch((err) => console.error(colors.red("Connection error"), err));
   } catch (error) {
