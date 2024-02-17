8104742423448242049582ba69ab933b4c70b288
Tilak Khatri <96524338+TilakKhatri@users.noreply.github.com>
2024-02-17 18:51:35 +0545

    Create Empty.tsx

---
+++ admin-ecommerce/src/components/ui/Empty.tsx
@@ -0,0 +1,9 @@
+function Empty() {
+  return (
+    <div className="w-full table-auto px-2 flex justify-center items-center h-screen">
+      <h1>empty </h1>
+    </div>
+  );
+}
+
+export default Empty;
