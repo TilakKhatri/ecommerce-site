3febfe2bc59833c827840a7ac842b6c62ce796a2
Tilak Khatri <96524338+TilakKhatri@users.noreply.github.com>
2024-02-17 18:52:46 +0545

    Create product.ts

---
+++ admin-ecommerce/src/types/product.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+  _id: string;
+  name: string;
+  category: string;
+  slug: string;
+  description: string;
+  quantity: number;
+  price: number;
+  rating: number;
+  isActive: boolean;
+  image: string;
+  created: string;
+}
